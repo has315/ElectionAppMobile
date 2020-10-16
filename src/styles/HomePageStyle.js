f58eb120fc6431f64b3b60aff15d3d20d1e1d385
zrilman <dragec.96@live.com>
2020-10-16 22:43:25 +0200

    Add HomePage

--- src/styles/HomePageStyle.js
+++ src/styles/HomePageStyle.js
@@ -1,13 +1,9 @@
 import {StyleSheet} from 'react-native';
+import colors from './BaseStyle';
 
 const style = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  votesContainer: {
-    backgroundColor: 'red',
   },
 });
 
