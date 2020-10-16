21c7ab3fcec89d108336b61176bb127126f08c15
zrilman <dragec.96@live.com>
2020-10-16 22:43:12 +0200

    Add VotesContainer

--- src/containers/VotesContainer.js
+++ src/containers/VotesContainer.js
@@ -1,9 +1,21 @@
 import React from 'react';
-import {Text} from 'react-native';
+import {View, Text, FlatList} from 'react-native';
 import VoteCard from '../components/VoteCard';
+import style from '../styles/VotesContainerStyle';
 
-const VotesContainer = (data) => {
-  data.map((vote) => <VoteCard vote={vote} />);
+const VotesContainer = ({votes}) => {
+  const renderVoteCard = ({item}) => {
+    return <VoteCard vote={item} />;
+  };
+
+  return (
+    <FlatList
+      style={style.container}
+      data={votes}
+      renderItem={renderVoteCard}
+      keyExtractor={(vote) => vote.vote_id.toString()}
+    />
+  );
 };
 
 export default VotesContainer;
---
+++ src/styles/VotesContainerStyle.js
@@ -0,0 +1,14 @@
+import {StyleSheet} from 'react-native';
+import colors from './BaseStyle';
+
+const style = StyleSheet.create({
+  container: {
+    flex: 4,
+    flexDirection: 'column',
+    backgroundColor: 'transparent',
+    padding: 5,
+    margin: 5,
+  },
+});
+
+export default style;
