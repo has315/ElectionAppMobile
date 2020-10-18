fda7c57346e8d57253942096f8a6305e93eb91f2
zrilman <dragec.96@live.com>
2020-10-18 15:22:55 +0200

    Load votes from api

--- src/containers/HomePage.js
+++ src/containers/HomePage.js
@@ -5,12 +5,28 @@ import SearchBar from '../components/SearchBar';
 import VotesContainer from '../containers/VotesContainer';
 import VotesContext from '../context/VotesContext';
 import DataUtils from '../utils/data';
+import VotesAPI from '../api/VotesAPI';
 
 const HomePage = ({navigation}) => {
   const [votes, setVotes] = useState([]);
 
   useEffect(() => {
-    setVotes(DataUtils.data);
+    let dir = 'next';
+    let config = {
+      headers: {authorization: DataUtils.user.token},
+      params: {id: DataUtils.user.id, start: '0', direction: dir},
+    };
+
+    VotesAPI.getVotes({}, config)
+      .then((resp) => {
+        let response = 'response';
+        if (response in resp) {
+          setVotes(resp.response);
+        } else {
+          console.log(resp);
+        }
+      })
+      .catch((err) => console.error(err));
   }, []);
 
   return (
