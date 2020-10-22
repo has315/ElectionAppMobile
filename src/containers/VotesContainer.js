a5eedea1337b692e7a6b3ec3e44941e1b3c636c5
zrilman <dragec.96@live.com>
2020-10-22 13:58:00 +0200

    Add loader

--- src/containers/VotesContainer.js
+++ src/containers/VotesContainer.js
@@ -1,8 +1,9 @@
 import React, {useContext} from 'react';
-import {FlatList} from 'react-native';
+import {FlatList, ActivityIndicator, View} from 'react-native';
 import VoteCard from '../components/VoteCard';
-import VotesContext from '../context/VotesContext';
+import VotesContext from '../context/DataContext';
 import style from '../styles/VotesContainerStyle';
+import colors from '../styles/BaseStyle';
 
 const VotesContainer = () => {
   const {votes} = useContext(VotesContext);
@@ -10,13 +11,17 @@ const VotesContainer = () => {
     return <VoteCard vote={item} />;
   };
 
-  return (
+  return votes.length > 0 ? (
     <FlatList
       style={style.container}
       data={votes}
       renderItem={renderVoteCard}
       keyExtractor={(vote) => vote.vote_id.toString()}
     />
+  ) : (
+    <View style={style.container}>
+      <ActivityIndicator size="large" color={colors.accent} />
+    </View>
   );
 };
 
