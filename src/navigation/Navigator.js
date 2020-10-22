9a4baa2ba9906b1c5396ed56a7794ad90c3905e1
zrilman <dragec.96@live.com>
2020-10-22 12:43:14 +0200

    Add logout support on back button

--- src/navigation/Navigator.js
+++ src/navigation/Navigator.js
@@ -1,13 +1,29 @@
 import React from 'react';
 import {createStackNavigator} from '@react-navigation/stack';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 import HomePage from '../containers/HomePage';
 import LoginPage from '../containers/LoginPage';
+import LoginService from '../services/LoginService';
 
 const Stack = createStackNavigator();
 
+const screenOptions = {
+  headerLeft: ({canGoBack, onPress}) =>
+    canGoBack && (
+      <Icon
+        name="chevron-left"
+        onPress={() => {
+          onPress();
+        }}
+        color="#333"
+        size={40}
+      />
+    ),
+};
+
 const Navigator = () => {
   return (
-    <Stack.Navigator initialRouteName="Login">
+    <Stack.Navigator initialRouteName="Login" screenOptions={screenOptions}>
       <Stack.Screen
         name="Login"
         component={LoginPage}
