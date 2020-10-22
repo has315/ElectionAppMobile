1fe9d7597cb2aa3a7bd4161de58d835686ee99f6
zrilman <dragec.96@live.com>
2020-10-22 07:57:45 +0200

    Add LoginService interface

---
+++ src/services/LoginService.js
@@ -0,0 +1,10 @@
+// import {pbkdf2Sync} from "pbkdf2"
+
+// const hashValue = async (value, salt) => {
+//    const hashedValue =  await pbkdf2Sync(value, salt, 4000, 256, "sha1");
+//    return hashedValue
+// }
+
+const hashValue = async (value, salt) => {};
+
+export default hashValue;
