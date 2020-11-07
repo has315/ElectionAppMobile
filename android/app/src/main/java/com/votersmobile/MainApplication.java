4ec87136c1724b468f58179bf7a2c7b996bfeada
zrilman <dragec.96@live.com>
2020-11-07 17:40:29 +0100

    Change package to com.rnpbkdf2

--- android/app/src/main/java/com/votersmobile/MainApplication.java
+++ android/app/src/main/java/com/votersmobile/MainApplication.java
@@ -4,7 +4,7 @@ import android.app.Application;
 import android.content.Context;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
-import rnpbkdf2.PBKDF2Package;
+import com.rnpbkdf2.PBKDF2Package;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
